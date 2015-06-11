93059af34cebf09c2567914f6f5fa052e5a7c866
nwn <ducnghiuit@gmail.com>
2015-06-11 15:06:36 +0700

    Basic Ajax

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610082919) do
+ActiveRecord::Schema.define(version: 20150610092418) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
