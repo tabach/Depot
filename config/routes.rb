0a419b7bb28ed25adce614368f6b235fa518b763
nwn <ducnghiuit@gmail.com>
2015-06-11 15:49:16 +0700

    D/Increase Ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :line_items
+   resources :line_items do
+    put 'decrease', on: :member
+    put 'increase', on: :member
+  end
+
   resources :carts
   get 'store/index'
 
